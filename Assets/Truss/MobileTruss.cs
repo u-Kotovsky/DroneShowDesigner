fa3cbcb11f95be27a7463c0beeab29dbe801a6f6
u-Kotovsky <id.u.kotovsky@gmail.com>
2025-10-27 23:53:01 +0300

    Remove override for rotation method in truss

--- Assets/Truss/MobileTruss.cs
+++ Assets/Truss/MobileTruss.cs
@@ -14,28 +14,6 @@ namespace Truss
             MaxAngle = 270;
         }
         
-        // I am going crazy and it does not work as intended
-        public void WriteDmxRotation(int offset, Vector3 rotation)
-        {
-            xRotationCoarse = Utility.GetCoarse(rotation.x, MinAngle, MaxAngle);
-            xRotationFine = Utility.GetFine(rotation.x, MinAngle, MaxAngle);
-        
-            yRotationCoarse = Utility.GetCoarse(rotation.y, MinAngle, MaxAngle);
-            yRotationFine = Utility.GetFine(rotation.y, MinAngle, MaxAngle);
-        
-            zRotationCoarse = Utility.GetCoarse(rotation.z, MinAngle, MaxAngle);
-            zRotationFine = Utility.GetFine(rotation.z, MinAngle, MaxAngle);
-        
-            Buffer[offset + 2] = xRotationCoarse;
-            Buffer[offset + 3] = xRotationFine;
-    
-            Buffer[offset + 0] = yRotationCoarse;
-            Buffer[offset + 1] = yRotationFine;
-    
-            Buffer[offset + 4] = zRotationCoarse;
-            Buffer[offset + 5] = zRotationFine;
-        }
-        
         public new byte[] GetDmxData()
         {
             WriteDmxPosition(0, transform.position);
