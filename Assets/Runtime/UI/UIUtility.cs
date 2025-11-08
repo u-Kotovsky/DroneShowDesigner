81188c4312964cd32300bd83209d94ae4b2880b1
u-Kotovsky <id.u.kotovsky@gmail.com>
2025-11-08 12:33:14 +0300

    Implement AddRect, SetAllStetch, AddItemToList, fix text scaling

--- Assets/Runtime/UI/UIUtility.cs
+++ Assets/Runtime/UI/UIUtility.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 namespace Runtime.UI
 {
-    public class UIUtility
+    public abstract class UIUtility
     {
         public static void AddButton(Sprite defaultUISprite, RectTransform rect, string title, Color buttonColor, Color textColor,
             Action<Button> callback)
@@ -16,65 +16,108 @@ namespace Runtime.UI
 
         public static Button AddButton(Sprite defaultUISprite, RectTransform rect, string title, Color buttonColor, Color textColor)
         {
-            GameObject obj = new GameObject($"Button ({title})");
+            var obj = AddRect(rect, $"Button ({title})");
             
-            var button = obj.AddComponent<Button>();
+            var button = obj.gameObject.AddComponent<Button>();
             obj.transform.SetParent(rect);
 
-            var image = obj.AddComponent<Image>();
+            var image = obj.gameObject.AddComponent<Image>();
             image.sprite = defaultUISprite;
-            
-            var colors = button.colors;
-            colors.normalColor = buttonColor;
-            button.colors = colors;
             button.targetGraphic = image;
             button.image.type = Image.Type.Sliced;
             button.image.pixelsPerUnitMultiplier = 1.5f;
+            
+            var colors = button.colors;
+            colors.normalColor = buttonColor;
+            colors.highlightedColor = buttonColor * 1.1f;
+            colors.selectedColor = buttonColor * .9f;
+            colors.pressedColor = buttonColor * .8f;
+            colors.disabledColor = buttonColor * 0.1f;
+            button.colors = colors;
             
             var text = AddText(obj.GetComponent<RectTransform>(), title, textColor);
             text.alignment = TextAlignmentOptions.Center;
             text.horizontalAlignment = HorizontalAlignmentOptions.Center;
             text.verticalAlignment = VerticalAlignmentOptions.Middle;
-            text.enableAutoSizing = true;
-            text.fontSizeMax = 24;
-            text.fontSizeMin = 7;
 
-            // I don't know why other stuff doesn't set rect to stretch, so I use this for now
-            var layout = button.gameObject.AddComponent<HorizontalLayoutGroup>();
-            layout.childAlignment = TextAnchor.MiddleCenter;
-            
-            //text.rectTransform.rect.Set();
-            //var textRect = text.gameObject.GetComponent<RectTransform>();
-            //textRect.anchorMax.Set(1f, 1f);
-            //textRect.anchorMin.Set(0f, 0f);
-            //textRect.sizeDelta.Set(0f, 0f);
-            //textRect.offsetMax.Set(0f, 0f);
-            //textRect.offsetMin.Set(0f, 0f);
-            //textRect.pivot = new Vector2(0.5f, 0.5f);
-            //textRect.anchoredPosition = new Vector2(1f, 1f);
-            //textRect.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Bottom, 0, 0);
-            //textRect.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Right, 0, 0);
-            
-            //text.rectTransform.anchorMin = Vector2.zero;
-            //text.rectTransform.anchorMax = Vector2.one;
-            //text.rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 0f, 0f);
-            //text.rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Right, 0f, 0f);
-            //text.rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 0f, 0f);
-            //text.rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Bottom, 0f, 0f);
+            SetAllStretch(text.rectTransform, 3, 3, -3, -3);
             
             return button;
         }
 
         public static TextMeshProUGUI AddText(RectTransform rect, string text, Color textColor)
         {
-            var obj = new GameObject("Text");
-            obj.transform.SetParent(rect, false);
+            var obj = AddRect(rect, "Text");
             
-            var component = obj.AddComponent<TextMeshProUGUI>();
+            var component = obj.gameObject.AddComponent<TextMeshProUGUI>();
             component.text = text;
             component.color = textColor;
+            component.enableAutoSizing = true;
+            component.fontSizeMax = 36;
+            component.fontSizeMin = 7;
             
             return component;
+        }
+
+        public static RectTransform AddRect(RectTransform parent, string name = "GameObject", bool worldPositionStays = false)
+            => AddRect(parent.transform, name, worldPositionStays);
+
+        public static RectTransform AddRect(GameObject parent, string name = "GameObject", bool worldPositionStays = false)
+            => AddRect(parent.transform, name, worldPositionStays);
+
+        public static RectTransform AddRect(Transform parent, string name = "GameObject", bool worldPositionStays = false)
+        {
+            var rectObj = new GameObject(name);
+            rectObj.transform.SetParent(parent.transform, worldPositionStays);
+            var rect = rectObj.AddComponent<RectTransform>();
+            return rect;
+        }
+
+        
+        public static RectTransform SetAllStretch(RectTransform rect, Vector4 offset = new())
+        {
+            // Fill the space
+            rect.anchorMin = Vector2.zero;
+            rect.anchorMax = Vector2.one;
+            rect.pivot = new Vector2(0.5f, 0.5f); // Center pivot for consistent behavior
+            rect.offsetMin = new Vector2(offset.x, offset.y); // Left, bottom
+            rect.offsetMax = new Vector2(offset.z, offset.w); // Right, top
+
+            return rect;
+        }
+        
+        public static RectTransform SetAllStretch(RectTransform rect, float x = 0, float y = 0, float z = 0, float w = 0)
+            => SetAllStretch(rect, new Vector4(x, y, z, w));
+        
+        public static RectTransform CreateList(Transform parent, string name)
+        {
+            var listRect = AddRect(parent, name);
+            var listLayout = listRect.gameObject.AddComponent<VerticalLayoutGroup>();
+            listLayout.childForceExpandWidth = true;
+            listLayout.childForceExpandHeight = false;
+            listLayout.childControlHeight = false;
+            
+            return listRect;
+        }
+        
+        // List is vertical
+        public static RectTransform AddItemToList(Transform parent, int internalIndex, float height, params string[] values)
+        {
+            var elementRect = AddRect(parent, $"Element {internalIndex}");
+            elementRect.gameObject.AddComponent<HorizontalLayoutGroup>();
+            elementRect.sizeDelta = new Vector2(0, height);
+            
+            var image = elementRect.gameObject.AddComponent<Image>();
+            image.color = internalIndex % 2 == 0 ? Color.white * 0.35f : Color.white * 0.05f;
+        
+            foreach (var value in values)
+            {
+                var subElementRect = AddRect(elementRect, value);
+                var subElementText = AddText(subElementRect, value, Color.white);
+                SetAllStretch(subElementText.rectTransform, Vector4.zero);
+            }
+                
+            return elementRect;
         }
     }
 }
